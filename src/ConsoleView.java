44186ac15b18a5cbb43cb268c2c3a8772f67e326
Cris <crisjavi1307@gmail.com>
2026-01-20 09:24:19 -0600

    Controlador agregado en la vista

--- src/ConsoleView.java
+++ src/ConsoleView.java
@@ -1,13 +1,76 @@
 package src;
-
 import java.util.Scanner;
 
 
 public class ConsoleView {
     private Scanner scanner;
+    private CarRadio radio;
 
     public ConsoleView(CarRadio radio) {
         this.scanner = new Scanner(System.in);
+        this.radio = radio;
+    }
+
+    public void controller() {
+        boolean exit = false;
+        displayMenu();
+
+        int option = getOption();
+
+        switch (option) {
+            case 1:
+                radio.turnOn();
+                showMessage("Radio Encendida");
+                break;
+        
+            case 2:
+                if (radio.isOn()) {
+                    if (radio.getBand() == "AM") {
+                        radio.switchBand("FM");
+                        showMessage("Cambiado a FM");
+                    } else {
+                        radio.switchBand("AM");
+                        showMessage("Cambiado a AM");
+                    }
+                } else {
+                    showMessage("La radio está apagada.");
+                }
+                break;
+            case 3:
+                if (radio.isOn()) {
+                    radio.nextStation();
+                    showMessage("Frecuencia cambiada a " + radio.getCurrentStation());
+                } else {
+                    showMessage("La radio está apagada.");
+                }
+                break;
+            case 4:
+                if (radio.isOn()) {
+                    int buttonSaved = getButtonNumber();
+                    radio.saveStation(radio.getCurrentStation() + "");
+                    showMessage("Estación guardada en el botón " + (buttonSaved + 1));
+                } else {
+                    showMessage("La radio esta apagada");
+                }
+                break;
+            case 5:
+                int buttonSelected = getButtonNumber();
+                if (radio.isOn()) {
+                    radio.selectStation(radio.getCurrentStation() + "");
+                    showMessage("Estación seleccionada: " + radio.getCurrentStation());
+                    
+                } else {
+                    showMessage("La radio está apagada.");
+                }
+                break;
+            case 6:
+                radio.turnOff();
+                showMessage("Radio Apagada");
+                break;
+            default:
+                showMessage("Opcion invalida");
+                break;
+        }
     }
 
     public void displayMenu() {
@@ -42,11 +105,12 @@ public class ConsoleView {
 
     public int getButtonNumber() {
         System.out.print("Seleccione una de las estaciones guardadas (1-12): ");
-        int estacionGuardada = scanner.nextInt() - 1; // Ajuste para índice de array
+        int estacionGuardada = scanner.nextInt() - 1;
         return estacionGuardada;
     }
 
     public void showMessage(String message) {
         System.out.println(message);
     }
+    
 }
